93da307f0338d77557d622a165e2175921167f97
Ricart <ricarteusuo11@gmail.com>
2020-02-25 13:15:33 -0300

    fazendo transição entre as telas de login e cadastro

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ root.title("DP Systems - Acess Panel")
 root.geometry("600x300")
 root.configure(background="white")
 root.resizable(width=False, height=False)
+# root.iconbitmap(default="icons/LogoIcon.ico")
 # Caso queira deixar transparente
 # root.atributes("-alpha", 0.9)
 
@@ -32,14 +33,57 @@ UserEntry.place(x=150, y=110)
 PassLabel = Label(RightFrame, text="Password:", font=("Century gothic", 20), bg="MIDNIGHTBLUE", fg="White")
 PassLabel.place(x=5, y=150)
 
-PassEntry = ttk.Entry(RightFrame, width=25)
+PassEntry = ttk.Entry(RightFrame, width=25, show="*")
 PassEntry.place(x=150, y=160)
 
 # Botoes
 LoginButton = ttk.Button(RightFrame, text="Login", width=25)
 LoginButton.place(x=100, y=225)
 
-RegisterButton = ttk.Button(RightFrame, text="Register", width=20)
+
+def register():
+    # Removendo Widgets de Login
+    LoginButton.place(x=5000)
+    RegisterButton.place(x=5000)
+
+    # Inserindo Widgets de Cadastro
+    NameLabel = Label(RightFrame, text="Name:", font=("Century gothic", 20), bg="MIDNIGHTBLUE", fg="White")
+    NameLabel.place(x=5, y=5)
+
+    NameEntry = ttk.Entry(RightFrame, width=25)
+    NameEntry.place(x=150, y=15)
+
+    EmailLabel = Label(RightFrame, text="Email:", font=("Century gothic", 20), bg="MIDNIGHTBLUE", fg="White")
+    EmailLabel.place(x=5, y=50)
+
+    EmailEntry = ttk.Entry(RightFrame, width=25)
+    EmailEntry.place(x=150, y=60)
+
+    # Inserindo botões de cadastro
+    Register = ttk.Button(RightFrame, text="Register", width=25)
+    Register.place(x=100, y=225)
+
+    def backToLogin():
+        # Removendo widgets de cadastro
+        NameLabel.place(x=5000)
+        NameEntry.place(x=5000)
+
+        EmailLabel.place(x=5000)
+        EmailEntry.place(x=5000)
+
+        # Removendo botões da tela de cadastro
+        Back.place(x=5000)
+        Register.place(x=5000)
+
+        # Readicionando botões de login
+        LoginButton.place(x=100)
+        RegisterButton.place(x=125)
+
+    Back = ttk.Button(RightFrame, text="Back", width=20, command=backToLogin)
+    Back.place(x=125, y=260)
+
+
+RegisterButton = ttk.Button(RightFrame, text="Register", width=20, command=register)
 RegisterButton.place(x=125, y=260)
 
 root.mainloop()
